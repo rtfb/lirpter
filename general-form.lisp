c8e3ca31760ae65695f3f93b9d9f387284080d63
Vytautas Šaltenis <vytas@rtfb.lt>
2017-12-09 14:16:44 +0200

    Initial implementation
    Seems to be working, but a bit difficult to say with the bare bones pure
    essence of Lisp. Will add some more features to make it better interact
    with the unpure Universe.

--- general-form.lisp
+++ general-form.lisp
@@ -1,22 +1,25 @@
-(defun lambdap (x)
-  (eq (car x) 'lambda))
-
-(defun do-lambda (x)
-  (format t "lambda!")
-  x)
-
-(defun do-symbol-lookup (x)
-  x)
-
-(defun do-apply (x)
-  x)
-
-(defun eval-expr (x)
+(defun eval-expr (x env)
   (cond
     ((numberp x)
-        (+ x x))
+     (+ x x))
     ((symbolp x)
-        (do-symbol-lookup x))
-    ((lambdap x)
-        (do-lambda x))
-    (t (do-apply x))))
+     (funcall env x))
+    ((eq (car x) 'lambda)
+     (lambda (arg)
+       (eval-expr (caddr x) (lambda (y)
+                              (if (eq (caadr x) y)
+                                arg
+                                (funcall env y))))))
+    (t (funcall (eval-expr (car x) env)
+                (eval-expr (cadr x) env)))))
+
+; $ sbcl --load general-form.lisp
+; This is SBCL 1.3.3.debian, an implementation of ANSI Common Lisp.
+; [...]
+; * (eval-expr '(lambda (z) z) (lambda (y) (error "unbound ~s" y)))
+; 
+; #<CLOSURE (LAMBDA (ARG) :IN EVAL-EXPR) {1003A88C6B}>
+; * (eval-expr '((lambda (z) z) 7) (lambda (y) (error "unbound ~s" y)))
+; 
+; 14
+; * 
